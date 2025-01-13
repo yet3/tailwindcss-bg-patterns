f60dcb16db8bf7c1cd00587bc3ba569259e099a6
Max Kasperowicz <yet3.dev@gmail.com>
2025-01-13 23:23:15 +0100

    feat: add links

---
+++ example/src/common/BuyMeCoffee.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Anchor from './Anchor.svelte';
+</script>
+
+<Anchor href="https://ko-fi.com/W7W618YWH4" ariaLabel="Support me on ko-fi (opnes in a new tab)">
+	<img
+		style="border:0px;height:32px;"
+		src="https://storage.ko-fi.com/cdn/kofi5.png?v=6"
+		alt="Buy Me a Coffee at ko-fi.com"
+	/>
+</Anchor>
